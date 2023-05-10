748c14fa9fa0699bf4d7a7905c1344193d994e67
Myeong A Kim <113976428+myeongakim7@users.noreply.github.com>
2023-05-10 16:13:43 +0900

    useState로 상태 관리하기
    ﻿리액트는 state로 값을 변경할 때만 화면이 업데이트 된다는 점이 중요

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import ColorButton from "./components/ColorButton";
 import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
   let name = "김명아";
@@ -31,6 +32,18 @@ function App() {
     display: "inline-block",
     fontSize: "16px",
   };
+  let count = 0;
+  let addCount = () => {
+    count++;
+    console.log(count);
+  };
+  // useState
+  // const [ 변수명, 변경함수명 ] = useState(초기값);
+  const [btn, setBtn] = useState(0);
+  let addBtn = () => {
+    setBtn(btn + 1);
+    console.log(btn);
+  };
   return (
     <div className="App">
       <p>안녕하세요. 저의 이름은 {name}입니다</p>
@@ -48,6 +61,16 @@ function App() {
       <ColorButton color="yellow" fonts="navy" text="장바구니" />
       <ColorButton color="purple" fonts="white" text="구매하기" />
       <Button color="pink" fonts="white" />
+      <h4>
+        useState가 필요한 이유 => 밑에처럼 하면 ui에 나타나지 않음(콘솔창에서만
+        버튼 누르면 숫자 상승)
+      </h4>
+      <p>{count}</p>
+      <button onClick={addCount}>button</button>
+      <h4>useState 사용으로 버튼 누르면 숫자 상승 </h4>
+      <p>{btn}</p>
+      <button onClick={addBtn}>UP</button>
+      {/* 실행 할 함수를 {}에 넣기*/}
     </div>
   );
 }
